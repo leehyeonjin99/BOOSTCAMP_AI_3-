1877ca4c2d5181584ecd2d71e852f96694103ade
leegyeonjin99 <reviewtest@ajou.ac.kr>
2022-01-27 12:43:43 +0900

    2022-06-27 수정 완료

--- 알고리즘/baekjoon/1697.py
+++ 알고리즘/baekjoon/1697.py
@@ -1,20 +1,17 @@
 import sys
 from collections import deque
-def bfs(start,brother):
-    visited=set()
+def bfs():
     result=deque([start])
     while result:
         n=result.popleft()
         if n==brother:
             print(dist[n])
             return
-        if n not in visited:
-            visited.add(n)
-            for next in [n-1,n+1,n*2]:
-                if 0<=next<=10**5 and next not in visited :
-                    dist[next]=dist[n]+1
-                    result.append(next)
+        for next in [n-1,n+1,n*2]:
+            if 0<=next<=10**5 and not dist[next] :
+                dist[next]=dist[n]+1
+                result.append(next)
 
 start,brother=map(int,sys.stdin.readline().split())
 dist=[0 for _ in range(10**5+1)]
-bfs(start,brother)
+bfs()
