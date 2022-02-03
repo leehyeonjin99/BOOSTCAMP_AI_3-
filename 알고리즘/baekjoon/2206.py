0fa314ba3bda3d97d7cd3b8ae1ff525d1a8e2105
leegyeonjin99 <reviewtest@ajou.ac.kr>
2022-02-04 01:25:35 +0900

    2022.02.03 시간초과 해결

--- 알고리즘/baekjoon/2206.py
+++ 알고리즘/baekjoon/2206.py
@@ -1,56 +1,40 @@
 import sys
 from collections import deque
+
+d=[0,0,1,-1]
+
 N,M=map(int,sys.stdin.readline().split())
 board=[]
 for _ in range(N):
     board.append(list(map(int,(' '.join(sys.stdin.readline()).split()))))
 
-visited=[]
-dist=[[sys.maxsize for _ in range(M)] for _ in range(N)]
-dist[0][0]=1
-que=deque([[[0,0],1]])
+visited=[[[0 for _ in range(M)] for _ in range(N)] for _ in range(2)]
+visited[1][0][0]=1
+que=deque([[0,0,1]])
 
 def check_wall(index):
     return bool(board[index[0]][index[1]])
 
 while que:
-    index,hammer=que.popleft()
-    if index not in visited:
-        visited.append(index)
-        next_index=[]
-        if index[0]>0:
-            wall=check_wall([index[0]-1,index[1]])
-            if wall:
-                if hammer:
-                    next_index.append([[index[0]-1,index[1]],hammer-1])
-            else:
-                next_index.append([[index[0]-1,index[1]],hammer])        
-        if index[0]<N-1:
-            wall=check_wall([index[0]+1,index[1]])
-            if wall:
-                if hammer:
-                    next_index.append([[index[0]+1,index[1]],hammer-1])
-            else:
-                next_index.append([[index[0]+1,index[1]],hammer])
-        if index[1]>0:
-            wall=check_wall([index[0],index[1]-1])
-            if wall:
-                if hammer:
-                    next_index.append([[index[0],index[1]-1],hammer-1])
-            else:
-                next_index.append([[index[0],index[1]-1],hammer])
-                
-        if index[1]<M-1:
-            wall=check_wall([index[0],index[1]+1])
-            if wall:
-                if hammer:
-                    next_index.append([[index[0],index[1]+1],hammer-1])
-            else:
-                next_index.append([[index[0],index[1]+1],hammer])
+    x,y,hammer=que.popleft()
 
-        for next, next_hammer in next_index:
-            if next not in visited:
-                dist[next[0]][next[1]]=dist[index[0]][index[1]]+1
-                que.append([next,next_hammer])
+    for i in range(4):
+        next_x=x+d[i]
+        next_y=y+d[3-i]
 
-print(dist[N-1][M-1] if dist[N-1][M-1]<sys.maxsize else -1)
+        if 0<=next_x<N and 0<=next_y<M:
+            if check_wall([next_x,next_y]) and hammer==1:
+                visited[0][next_x][next_y]=visited[hammer][x][y]+1
+                que.append([next_x,next_y,0])
+            elif not check_wall([next_x,next_y]) and visited[hammer][next_x][next_y]==0:
+                visited[hammer][next_x][next_y]=visited[hammer][x][y]+1
+                que.append([next_x,next_y,hammer])  
+
+if visited[0][N-1][M-1]!=0 and visited[1][N-1][M-1]!=0:
+    print(min(visited[0][N-1][M-1], visited[1][N-1][M-1]))
+elif visited[0][N-1][M-1]!=0:
+    print(visited[0][N-1][M-1])
+elif visited[1][N-1][M-1]!=0:
+    print(visited[1][N-1][M-1])
+else:
+    print(-1)
