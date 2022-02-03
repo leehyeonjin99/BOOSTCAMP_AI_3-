a45c4f1aacc34396b3593ccbec5b79e347faf19c
leegyeonjin99 <reviewtest@ajou.ac.kr>
2022-02-04 01:48:38 +0900

    2022.02.03 시간초과 수정

--- 알고리즘/baekjoon/2206.py
+++ 알고리즘/baekjoon/2206.py
@@ -11,30 +11,26 @@ for _ in range(N):
 visited=[[[0 for _ in range(M)] for _ in range(N)] for _ in range(2)]
 visited[1][0][0]=1
 que=deque([[0,0,1]])
+def dfs():
+    while que:
+        x,y,hammer=que.popleft()
 
-def check_wall(index):
-    return bool(board[index[0]][index[1]])
+        if x==N-1 and y==M-1:
+            print(visited[hammer][x][y])
+            return
 
-while que:
-    x,y,hammer=que.popleft()
+        for i in range(4):
+            next_x=x+d[i]
+            next_y=y+d[3-i]
 
-    for i in range(4):
-        next_x=x+d[i]
-        next_y=y+d[3-i]
-
-        if 0<=next_x<N and 0<=next_y<M:
-            if check_wall([next_x,next_y]) and hammer==1:
-                visited[0][next_x][next_y]=visited[hammer][x][y]+1
-                que.append([next_x,next_y,0])
-            elif not check_wall([next_x,next_y]) and visited[hammer][next_x][next_y]==0:
-                visited[hammer][next_x][next_y]=visited[hammer][x][y]+1
-                que.append([next_x,next_y,hammer])  
-
-if visited[0][N-1][M-1]!=0 and visited[1][N-1][M-1]!=0:
-    print(min(visited[0][N-1][M-1], visited[1][N-1][M-1]))
-elif visited[0][N-1][M-1]!=0:
-    print(visited[0][N-1][M-1])
-elif visited[1][N-1][M-1]!=0:
-    print(visited[1][N-1][M-1])
-else:
+            if 0<=next_x<N and 0<=next_y<M:
+                if board[next_x][next_y]==1 and hammer==1:
+                    visited[0][next_x][next_y]=visited[hammer][x][y]+1
+                    que.append([next_x,next_y,0])
+                elif board[next_x][next_y]==0 and visited[hammer][next_x][next_y]==0:
+                    visited[hammer][next_x][next_y]=visited[hammer][x][y]+1
+                    que.append([next_x,next_y,hammer])  
     print(-1)
+    return
+
+dfs()
