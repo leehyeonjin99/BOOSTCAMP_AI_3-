3db27fb934a8862ffd1b6b545de127fd6c02f289
leehyeonjin99 <57162812+leehyeonjin99@users.noreply.github.com>
2022-02-12 15:20:26 +0900

    Create 거리두기_확인하기.py

---
+++ 알고리즘/Programmers/거리두기_확인하기.py
@@ -0,0 +1,24 @@
+def solution(places):
+    answer=[int(dist_check(case)) for case in places]
+    return answer
+
+import math
+
+def dist_check(case):
+    check=True
+    for i in range(5):
+        for j in range(5):
+            if case[i][j]=='P':
+                if i+1<5 and case[i+1][j]=='P':
+                    return False
+                elif i+2<5 and case[i+1][j]!='X' and case[i+2][j]=='P':
+                    return False
+                elif j+1<5 and case[i][j+1]=='P':
+                    return False
+                elif j+2<5 and case[i][j+1]!='X' and case[i][j+2]=='P':
+                    return False
+                elif i+1<5 and j+1<5 and case[i+1][j+1]=='P' and (case[i+1][j]!='X' or case[i][j+1]!='X'):
+                    return False
+                elif i+1<5 and j-1>=0 and case[i+1][j-1]=='P' and (case[i+1][j]!='X' or case[i][j-1]!='X'):
+                    return False
+    return True
