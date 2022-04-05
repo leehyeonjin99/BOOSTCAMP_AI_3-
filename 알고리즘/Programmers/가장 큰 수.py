555325a0b52a4e19e71c68e4ee5da098167282c8
leehyeonjin99 <57162812+leehyeonjin99@users.noreply.github.com>
2022-04-05 21:21:43 +0900

    Create 가장 큰 수.py

---
+++ 알고리즘/Programmers/가장 큰 수.py
@@ -0,0 +1,4 @@
+def solution(numbers):
+    numbers = list(map(str, numbers)) # 사전식으로 앞자리 부터 확인하기 위해서 str로 변경한다.
+    numbers.sort(key = lambda x : x*3, reverse = True) # 1000이하의 숫자이므로 3자리에 맞춰 비교하기 위해서 x*3을 한다.
+    return str(int(''.join(numbers))) # 0000은 0으로 표현하기 위해 int로 type 설정 후 문제에서 원하는 answer는 str이므로 type을 재설정 한다.
