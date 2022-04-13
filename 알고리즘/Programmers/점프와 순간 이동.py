d4f9a099157d74e34649f92a548aea6ba94cc8ea
leehyeonjin99 <57162812+leehyeonjin99@users.noreply.github.com>
2022-04-14 08:58:13 +0900

    2022.04.14 다시 풀기

---
+++ 알고리즘/Programmers/점프와 순간 이동.py
@@ -0,0 +1,11 @@
+from collections import deque
+
+def solution(n):
+    ans = 0
+    while n:
+        if n%2 == 0:
+            n = n//2
+        else:
+            n = n-1
+            ans+=1
+    return ans
