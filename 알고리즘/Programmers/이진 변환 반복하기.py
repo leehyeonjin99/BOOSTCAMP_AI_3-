8ddfb27d25a2a3d848f2cd2248a1cd26ebcaae46
leehyeonjin99 <57162812+leehyeonjin99@users.noreply.github.com>
2022-04-17 00:56:17 +0900

    2022.04.16

---
+++ 알고리즘/Programmers/이진 변환 반복하기.py
@@ -0,0 +1,9 @@
+def solution(s):
+    count, zero = 0, 0
+    while s!='1':
+        zero += s.count('0')
+        s = s.replace('0','')
+        s = bin(len(s))[2:]
+        count += 1
+    answer = [count, zero]
+    return answer
